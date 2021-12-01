157cb058e8475c217a429b84187ebfae7da15c7c
Sebastian Sandstig <sebastian.sandstig@learnet.se>
2021-12-01 09:48:07 +0100

    p1

--- p1_p2.py
+++ p1_p2.py
@@ -3,18 +3,13 @@ path = __file__ + "/../i1.txt"
 
 data = list(map(lambda xs: int(xs), open(path).read().split()))
 
-x = data.pop(0)
+x = data[-1]
 
 c = 0
-while True:
-  try:
-    ms = data[:3]
-    nx = sum(ms)
-    if nx > x and len(ms) == 3:
-      c += 1
-    x = nx
-    data.pop(0)
-  except:
-    break
+while data:
+  nx = sum(data[:3])
+  if nx > x: c += 1
+  x = nx
+  data.pop(0)
 
 print(c)
