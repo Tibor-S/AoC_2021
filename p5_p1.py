6c9df08dc10487516ffaf53085e1dde857372508
unknown <sebbbes2@gmail.com>
2021-12-05 16:42:21 +0100

    p5

---
+++ p5_p1.py
@@ -0,0 +1,21 @@
+from collections import defaultdict as dd
+
+path = __file__ + "/../i5.in"
+pipes = list(map(lambda xs: (tuple(map(int, xs[0].split(','))), tuple(map(int, xs[1].split(',')))), map(lambda xs: xs.split(' -> '), open(path).read().split('\n'))))
+grid = {}
+grid = dd(lambda: 0)
+
+for pipe in pipes:
+  strt = pipe[0]
+  end = pipe[1]
+  if strt[0] == end[0] or strt[1] == end[1]:
+    for x in range(min(strt[0], end[0]), max(strt[0], end[0]) + 1):
+      for y in range(min(strt[1], end[1]), max(strt[1], end[1]) + 1):
+        grid[(x,y)] += 1
+
+c = 0    
+for key in grid:
+  if grid[key] > 1:
+    c += 1
+
+print(c)
