8d1626f3a5b3e76fd407c5545e5a134f99287107
unknown <sebbbes2@gmail.com>
2021-12-14 17:15:58 +0100

    p14

--- p14_p2.py
+++ p14_p2.py
@@ -5,13 +5,23 @@ data = open(path).read().replace('->', ' ').split()
 count = [0 for i in range(ord('Z') - ord('A'))]
 rules = {}
 while data[1:]:
-  rules[data.pop()] = data.pop()
-polymer = list(data.pop())
-for n in range(10):
-  for i in range(len(polymer) - 1):
-    polymer.append(polymer.pop(0))
-    polymer.append(rules[polymer[-1] + polymer[0]])
-  polymer.append(polymer.pop(0))
-for c in polymer: count[ord(c) - 65] += 1
+  to = data.pop()
+  key = data.pop()
+  rules[key] = {"to": [key[0] + to, to + key[1]], "current": 0, "next": 0}
+for i in range(len(data[0]) - 1): 
+  count[ord(data[0][i]) - 65] += 1
+  rules[data[0][i:i+2:]]["current"] += 1
+count[ord(data[0][-1]) - 65] += 1
+
+
+for n in range(40):
+  for key in rules:
+    count[ord(rules[key]["to"][0][1]) - 65] += rules[key]["current"]
+    for to in rules[key]["to"]:
+      rules[to]["next"] += rules[key]["current"]
+    rules[key]["current"] = 0
+  for key in rules:
+    rules[key]["current"] = rules[key]["next"]
+    rules[key]["next"] = 0
 count = sorted([c for c in count if c != 0])
 print(count[-1] - count[0])
